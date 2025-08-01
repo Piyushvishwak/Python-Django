54be60d7e554a9f7cc76f49f76cd0f7076eb8772
piyush vishwakarma <piyushvish4@gmail.com>
2025-08-01 12:13:16 +0530

    templates in django

--- project0/views.py
+++ project0/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 
 
 def Home(request):
-    return render(request, 'index.html')
+    return render(request, 'website/index.html')
     # return HttpResponse("<h1>Hello world. I am learning Django.</h1>")
 
 def About(request):
-    return render(request, 'about.html')
+    return render(request, 'website/about.html')
 
 def Contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'website/contact.html')
