35eb84dd731b359c2ed90512143e8077083c76d3
piyush vishwakarma <piyushvish4@gmail.com>
2025-08-07 00:36:31 +0530

    add styling and database-connection

--- project1/recipes/models.py
+++ project1/recipes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    image_url = models.URLField(max_length=500, blank=True, null=True)  # ✅ allows blank
+
+    def __str__(self):
+        return self.title
--- project1/recipes/views.py
+++ project1/recipes/views.py
@@ -1,36 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
 def recipe_list(request):
-    recipes = [
-        {"id": 1, "title": "Pasta", "description": "Delicious creamy pasta", "image": "https://img.freepik.com/free-photo/penne-pasta-tomato-sauce-with-chicken-tomatoes-wooden-table_2829-19739.jpg"},
-        {"id": 2, "title": "Salad", "description": "Fresh vegetable salad", "image": "https://images.pexels.com/photos/1059905/pexels-photo-1059905.jpeg?cs=srgb&dl=pexels-chanwalrus-1059905.jpg&fm=jpg"},
-    ]
+    recipes = Recipe.objects.all()
     return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
 
 def recipe_detail(request, id):
-    recipes = [
-        {
-            "id": 1,
-            "title": "Pasta",
-            "description": "Delicious creamy pasta",
-            "ingredients": "Pasta, Cream, Cheese",
-            "instructions": "Boil pasta and mix with cream.",
-            "image": "https://img.freepik.com/free-photo/penne-pasta-tomato-sauce-with-chicken-tomatoes-wooden-table_2829-19739.jpg"
-        },
-        {
-            "id": 2,
-            "title": "Salad",
-            "description": "Fresh vegetable salad",
-            "ingredients": "Lettuce, Tomato, Cucumber",
-            "instructions": "Mix all veggies and serve.",
-            "image": "https://images.pexels.com/photos/1059905/pexels-photo-1059905.jpeg?cs=srgb&dl=pexels-chanwalrus-1059905.jpg&fm=jpg"
-        },
-    ]
-
-    recipe = None
-    for item in recipes:
-        if item["id"] == id:
-            recipe = item
-            break
+    recipe = get_object_or_404(Recipe, id=id)
     return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
-
